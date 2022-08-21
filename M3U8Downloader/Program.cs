e734883b7bcaa4fa5267af9c8686600b2768b97b
Dragon-0609 <reddragon.levelup@gmail.com>
2022-08-21 18:17:30 +0500

    Write output file name the name of m3u8 file

--- M3U8Downloader/Program.cs
+++ M3U8Downloader/Program.cs
@@ -19,6 +19,7 @@ namespace M3U8Downloader
 		private static void RunApp (string[] args)
 		{
 			int all = 0;
+			string name = "full";
 			if (args.Length == 0)
 			{
 				Console.WriteLine ("Set Main Link\nFor example:\nFrom https://de3.libria.fun/videos/media/ts/9210/6/480/4b74a2ffeb058591dfdc6caffd08953b.m3u8\nto  https://de3.libria.fun/videos/media/ts/9210/6/480/");
@@ -30,6 +31,7 @@ namespace M3U8Downloader
 					file = file.Replace ("\"", "");
 				}
 
+				name = Path.GetFileNameWithoutExtension (file);
 				Environment.CurrentDirectory = Path.GetDirectoryName (file);
 
 				string[] content = File.ReadAllText (file).Split (new char[] { '\n' }, StringSplitOptions.None)
@@ -39,10 +41,22 @@ namespace M3U8Downloader
 				all = DownloadEachSegment (main_url, content);
 				Console.WriteLine ("Downloading end");
 
-			} else
+			} else if (args.Length == 2)
 			{
 				all = int.Parse (args[0]);
-				Environment.CurrentDirectory = Path.GetDirectoryName (args[1]);
+				
+				string file = args[1];
+				if (file.StartsWith ("\"") && file.EndsWith ("\""))
+				{
+					file = file.Replace ("\"", "");
+				}
+
+				Environment.CurrentDirectory = Path.GetDirectoryName (file);
+				if (File.Exists (file))
+					name = Path.GetFileName (file);
+			} else
+			{
+				Console.WriteLine("Invalid length of arguments. Acceptable: 0 or 2 args");
 			}
 
 			/*
@@ -51,7 +65,7 @@ namespace M3U8Downloader
 			/*if (rename.Length == 0)
 				rename = "Без названия";*/
 			// RenameAll (rename, all);
-			Merge (all);
+			Merge (all, name);
 			Console.ReadKey ();
 		}
 
@@ -98,12 +112,15 @@ namespace M3U8Downloader
 			}
 		}
 
-		private static void Merge(int max){
-			RenameFile ("full.ts");
-			RenameFile ("output.mp4");
-			CombineMultipleFilesIntoSingleFile(max, "full.ts");
-			Console.WriteLine ("After merge, write to convert to mp4: \nffmpeg -i full.ts -c:v libx264 -preset medium -tune film -crf 23 -strict experimental -c:a aac -b:a 192k output.mp4");
-			
+		private static void Merge(int max, string name){
+			string file = $"{name}.ts";
+			RenameFile (file);
+			string mp4 = $"{name}.mp4";
+			RenameFile (mp4);
+			CombineMultipleFilesIntoSingleFile(max, file);
+			Console.WriteLine (
+				$"After merge, write to convert to mp4: \nffmpeg -i {file} -c:v libx264 -preset medium -tune film -crf 23 -strict experimental -c:a aac -b:a 192k {mp4}");
+
 		}
 
 		private static void RenameFile (string file, int count = 1)
